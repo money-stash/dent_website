71931b233262349d9f5d91be24fd7c820d26731d
maksEveline <maksimeveline@gmail.com>
2025-11-17 18:52:10 +0200

    big big update

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+
+@app.route("/prices")
+def prices():
+    return render_template("prices.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
